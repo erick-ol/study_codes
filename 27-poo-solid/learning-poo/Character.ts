53946c2a2712a39b4c13ad81ddfae3c4f9d21616
Erick Santos <erickosantos.dev@gmail.com>
2022-03-07 14:44:30 -0300

    encapsulation

--- 27-poo-solid/learning-poo/Character.ts
+++ 27-poo-solid/learning-poo/Character.ts
@@ -1,12 +1,44 @@
 export default class Character {
   constructor(
-    public name: string,
-    public energy: number,
-    public attack: number,
-    public defense: number,
+    private _name: string,
+    private _energy: number,
+    private _attack: number,
+    private _defense: number,
   ) {}
 
-  status(): string {
+  public get name(): string {
+    return this._name;
+  }
+
+  public get energy(): number {
+    return this._energy;
+  }
+
+  public set energy(energy: number) {
+    if (energy > 100) {
+      this._energy = 100;
+    } else {
+      this._energy = energy;
+    }
+  }
+
+  public get attack(): number {
+    return this._attack;
+  }
+
+  public set attack(attack: number) {
+    this._attack = attack;
+  }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  public set defense(defense: number) {
+    this._defense = defense;
+  }
+
+  public status(): string {
     return `Warrior:
   Name: ${this.name}
   Energy: ${this.energy.toFixed(1)}
@@ -14,34 +46,37 @@ export default class Character {
   Defense: ${this.defense.toFixed(1)}`;
   }
 
-  trainAttack(): void {
-    this.attack += Math.random() * 5;
-    this.energy -= Math.random() * 10;
+  public trainAttack(): void {
+    this.attack += this.random(7);
+    this.energy -= this.random(10);
     if (this.attack > 100) {
       this.attack = 100;
     }
   }
 
-  trainDefense(): void {
-    this.defense += Math.random() * 5;
-    this.energy -= Math.random() * 10;
+  public trainDefense(): void {
+    this.defense += this.random(5);
+    this.energy -= this.random(10);
     if (this.defense > 100) {
       this.defense = 100;
     }
   }
 
-  rest(hours: number): void {
-    this.energy += hours * Math.random() * 10;
-    if (this.energy > 100) this.energy = 100;
+  public rest(hours: number): void {
+    this.energy += hours * this.random(10);
   }
 
-  battle(): number {
-    const energyCost = Math.random() * 100;
+  public battle(): number {
+    const energyCost = this.random(100);
     this.energy -= energyCost;
     return energyCost;
   }
 
-  isAlive(): boolean {
+  public isAlive(): boolean {
     return this.energy >= 0;
+  }
+
+  private random(value: number): number {
+    return Math.random() * value;
   }
 }
