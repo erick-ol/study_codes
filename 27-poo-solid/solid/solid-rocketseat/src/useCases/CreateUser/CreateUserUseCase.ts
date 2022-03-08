7d500cbc53ce2c4e453d433e4f92ed9f0952bfa7
Erick Santos <erickosantos.dev@gmail.com>
2022-03-08 18:39:48 -0300

    SOLID Rocketseat

---
+++ 27-poo-solid/solid/solid-rocketseat/src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,35 @@
+import { User } from '../../entities/User';
+import { IMailProvider } from '../../providers/IMailProvider';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { ICreateUserRequestDTO } from './CreateUserDTO';
+
+export class CreateUserUseCase {
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider,
+  ) {}
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email,
+    );
+    if (userAlreadyExists) {
+      throw new Error('User already exists.');
+    }
+
+    const user = new User(data);
+    await this.usersRepository.save(user);
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: 'Equipe do App',
+        email: 'equipe@app.com',
+      },
+      subject: 'Desejamos boas vindas ao app',
+      body: '<p>Ja pode fazer login</p>',
+    });
+  }
+}
