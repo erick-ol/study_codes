90f8d229835c8c037b868c6e19746ee7963a1fd1
Erick Santos <erickosantos.dev@gmail.com>
2022-03-08 14:27:57 -0300

    Polimorfismo

---
+++ 27-poo-solid/polymorphism/class-sintax.ts
@@ -0,0 +1,96 @@
+// class Animal {
+//   constructor(public name: string) {}
+//   move() {
+//     console.log(`${this.name} está se movendo.`);
+//   }
+// }
+// class Bird extends Animal {
+//   move() {
+//     super.move();
+//     console.log(`${this.name} está voando.`);
+//   }
+// }
+// class Mammal extends Animal {
+//   move() {
+//     console.log(`${this.name} está andando.`);
+//   }
+// }
+
+// const a = new Animal('Tubarão');
+// const b = new Bird('Papagaio');
+// const m = new Mammal('Tatu');
+
+// const myMove = (animal: Animal) => {
+//   animal.move();
+// };
+// myMove(a);
+// myMove(b);
+// myMove(m);
+
+// /*
+// Saída:
+// Tubarão está se movendo.
+// Papagaio está se movendo.
+// Papagaio está voando.
+// Tatu está andando.
+// */
+
+// abstract class Animal {
+//   constructor(public name: string) {}
+//   abstract move(): void;
+// }
+// class Bird extends Animal {
+//   move() {
+//     console.log(`${this.name} está voando.`);
+//   }
+// }
+// class Mammal extends Animal {
+//   move() {
+//     console.log(`${this.name} está andando.`);
+//   }
+// }
+// class Fish extends Animal {
+//   move() {
+//     console.log(`${this.name} está nadando.`);
+//   }
+// }
+
+// const a = new Fish('Tubarão');
+// const b = new Bird('Papagaio');
+// const m = new Mammal('Tatu');
+
+// const myMove = (animal: Animal) => {
+//   animal.move();
+// };
+// myMove(a);
+// myMove(b);
+// myMove(m);
+
+// /*
+// Saída:
+// Tubarão está nadando.
+// Papagaio está voando.
+// Tatu está andando.
+// */
+
+class Employee {
+  private static employeeCount = 0;
+
+  constructor(public name: string) {
+    Employee.addEmployee();
+  }
+
+  private static addEmployee() {
+    this.employeeCount += 1;
+  }
+
+  static get employees() {
+    return this.employeeCount;
+  }
+}
+
+console.log(Employee.employees);
+const e1 = new Employee('Ronald');
+console.log(Employee.employees);
+const e2 = new Employee('Cíntia');
+console.log(Employee.employees);
